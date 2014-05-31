8fbc4521a9b7bacb93b015e7ba92d04d33e224e1
plugramme <plugramme+github@gmail.com>
2014-05-31 23:41:31 +0900

    Add SweetP Productions Cookie.app v3.0.31
    Add SweetP Productions Cookie.app v3.0.31

---
+++ Casks/cookie.rb
@@ -0,0 +1,7 @@
+class Cookie < Cask
+  url 'http://www.sweetpproductions.com/products/cookie/Cookie.dmg'
+  homepage 'http://sweetpproductions.com/'
+  version '3.0.31'
+  sha256 '1d75c1411288ad3b8be663d567a0d2d9b42b1a0534cc798e1eb62da844b810fa'
+  link 'Cookie.app'
+end
